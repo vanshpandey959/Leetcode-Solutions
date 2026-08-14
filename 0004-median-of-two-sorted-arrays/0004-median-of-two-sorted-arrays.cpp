601c86a8c304d8b1d5ed70175ddb12361e9b1fc5
Vansh Dutt Pandey <pvansh011@gmail.com>
2026-08-14 19:22:02 +0530

    Time: 0 ms (100%), Space: 95.1 MB (61.69%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -2,47 +2,34 @@ class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int n1 = nums1.size(), n2 = nums2.size();
-        int i = 0, j = 0;
+        if(n1 > n2) return findMedianSortedArrays(nums2, nums1);
+
+        int low = 0, high = n1;
         int n = n1+n2;
 
-        int ind1 = n/2;
-        int ind2 = ind1-1;
+        int left = (n1+n2+1)/2;
+        
+        while(low <= high) {
+            int mid1 = low + (high-low)/2;
+            int mid2 = left-mid1;
 
-        int ind1el = -1, ind2el = -1;
-        int cnt = 0;
+            int l1 = INT_MIN, l2 = INT_MIN;
+            int r1 = INT_MAX, r2 = INT_MAX;
 
-        while(i < n1 && j < n2) {
-            if(nums1[i] < nums2[j]) {
-                if(cnt == ind1) ind1el = nums1[i];
-                if(cnt == ind2) ind2el = nums1[i];
-                i++;
-                cnt++;
-            }
-            else {
-                if(cnt == ind1) ind1el = nums2[j];
-                if(cnt == ind2) ind2el = nums2[j];
-                j++;
-                cnt++;
-            }
-        }
-        while(i < n1) {
-            if(cnt == ind1) ind1el = nums1[i];
-            if(cnt == ind2) ind2el = nums1[i];
-            cnt++;
-            i++;
+            if(mid1 < n1) r1 = nums1[mid1];
+            if(mid2 < n2) r2 = nums2[mid2];
+
+            if(mid1-1 >= 0) l1 = nums1[mid1-1];
+            if(mid2-1 >= 0) l2 = nums2[mid2-1];
+
+            if(l1 <= r2 && l2 <= r1) {
+                if(n%2 == 1) return max(l1, l2);
+                return ((double)(max(l1,l2) + min(r1,r2))) / 2.0;
+            } 
+            else if(l1 > r2) high = mid1-1;
+            else low = mid1+1;
         }
 
-        while(j < n2) {
-            if(cnt == ind1) ind1el = nums2[j];
-            if(cnt == ind2) ind2el = nums2[j];
-            j++;
-            cnt++;
-        }
-
-        if(n%2 == 1) {
-            return ind1el;
-        }
-
-        return (double)((double)(ind1el + ind2el)) / 2.0;
+        return 0;
      }
 };
